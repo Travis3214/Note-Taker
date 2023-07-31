2c6617d970fef2b294421e02bbd0c5c7abf1b2ee
Travis <travis3214@yahoo.com>
2023-07-31 19:04:54 -0400

    added files needed and started naming constants

---
+++ server.js
@@ -0,0 +1,9 @@
+// Import express and fs //
+const express = require('express');
+const pass = require('pass');
+const fs = require('fs');
+
+// Define express //
+const app = express();
+
+// Define port //
