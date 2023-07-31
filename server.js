ee126a1dbffcdfc2161d63e7b18c87c0835e8424
Travis <travis3214@yahoo.com>
2023-07-31 19:30:35 -0400

    brought in helper files

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ const express = require('express');
 const pass = require('path');
 const fs = require('fs');
 
+// Bringing in helper functions for reading/writing/deleting // 
+const { readFromFile, readAndAppend, deleteFromFile } = require('./helpers/fsUtils');
+
+// Bringing in file for generating id number //
+const uuid = require('./helpers/uuid');
+
 // Define express //
 const app = express();
 
 // Define port //
 const PORT = process.env.PORT || 3001;
+
+// Creating an array for notes // 
+const notesArray = [];
+
